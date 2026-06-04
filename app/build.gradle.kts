0e83feef5f0851f489e34917c1cf7fbdcf60004a
ILYESwsa <jhgn671@gmail.com>
2026-06-04 08:12:41 +0100

    fix: change RepositoriesMode to PREFER_SETTINGS to allow JitPack resolution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -218,10 +218,6 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
     }
 }
 
-repositories {
-    maven { setUrl("https://jitpack.io") }
-}
-
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-analytics")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
 
     repositories {
         google()
